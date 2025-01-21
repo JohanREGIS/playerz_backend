02ebaa491c2dfc490fa184e60ce06a9b2cfdd16d
johanREGIS <johanjregis@gmail.com>
2025-01-21 21:56:50 +0100

    first commit

---
+++ playerzbackend/src/main/java/com/playerz/playerzbackend/PlayerzbackendApplication.java
@@ -0,0 +1,13 @@
+package com.playerz.playerzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerzbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerzbackendApplication.class, args);
+	}
+
+}
---
+++ playerzbackend/src/test/java/com/playerz/playerzbackend/PlayerzbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playerz.playerzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerzbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
